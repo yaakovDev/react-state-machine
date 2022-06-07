528f58e6c8396eb5880c34fb90fab9abcd771c89
yaakovDev <yaakovDevv@gmail.com>
2022-06-07 22:36:55 +0300

    commit-2

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -18,9 +18,12 @@ const trafficSignalMachine = createMachine({
       on: {NEXT: 'green'}
      },
 		green:{
-			 on: {NEXT: 'red'},
+			 on: {NEXT: 'orange_reverse'},
        entry: 'alertGreen'
 			},
+    orange_reverse:{
+        on: {NEXT: 'red'},
+      },
 	}//states	
 },
 {
@@ -43,8 +46,8 @@ export const TrafficSignal = () => {
   <div style={{display:'flex',flexDirection:'column',gap:'5px',padding:'5px'}}>
     { !signal.matches('red') &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
     { signal.matches('red') &&<div style={{backgroundColor:'red',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-    { !signal.matches('orange') &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-    { signal.matches('orange') &&<div style={{backgroundColor:'orange',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
+    { !['orange','orange_reverse'].some(signal.matches) &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
+    { ['orange','orange_reverse'].some(signal.matches) &&<div style={{backgroundColor:'orange',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
     { !signal.matches('green') && <div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
     { signal.matches('green') && <div style={{backgroundColor:'green',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
 
@@ -64,6 +67,38 @@ const pedestrianSignal = createMachine({
    green:{
       on: {NEXT: 'red'}
      },
+ }//states	
+});
+
+
+const crossWalkMachine = createMachine({
+ id:'cross-walk-machine',
+ initial:'stop',
+ states:{ 
+   stop:{
+     on:{NEXT:'pedestrian_walk'}
+   },
+   pedestrian_walk:{
+      //pedestrianSignal.send('NEXT')
+      on: {NEXT: 'pedestrian_stop'}
+     },
+  pedestrian_stop:{
+      //pedestrianSignal.send('NEXT')
+      on: {NEXT: 'car_standby'}
+     },
+  car_standby:{
+    //trafficSignalMachine.send('NEXT')
+    on: {NEXT: 'car_go'}
+    },
+  car_go:{
+      //trafficSignalMachine.send('NEXT')
+      on: {NEXT: 'car_stop'}
+    },
+  car_stop:{
+      //trafficSignalMachine.send('NEXT')
+      on:{NEXT:'pedestrian_walk'}
+    },
+
  }//states	
 });
 
