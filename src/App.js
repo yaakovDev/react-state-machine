9bd3ac23370b2777c8f7b74e36a6574153f650c0
yaakovDev <yaakovDevv@gmail.com>
2022-06-15 09:09:44 +0300

    commit-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,120 @@
+import {createContext , useState,useEffect} from "react";
 import './App.css';
-import { TrafficSignal,PedestrianTrafficSignal} from './TrafficSignal'
+import { TrafficSignal,PedestrianTrafficSignal,CrossWalk} from './TrafficSignal'
+import { createMachine,send,assign,spawn } from 'xstate';
+import { useMachine } from '@xstate/react';
+
+const trafficSignalMachine = createMachine({
+  id:'traffic-signal',
+  initial:'red',
+	states:{ 
+		red:{
+      after:{5000:'orange'},
+			on:{NEXT:'orange'}
+		},
+		orange:{
+      after:{1000:'green'},
+      on: {NEXT: 'green'}
+     },
+		green:{
+      after:{10000:'orange_reverse'},
+			on: {NEXT: 'orange_reverse'}
+ 			 },
+    orange_reverse:{
+      after:{1000:'red'},
+        on: {NEXT: 'red'}
+      },
+	}//states	
+});
+
+
+const pedestrianSignal = createMachine({
+  id:'pedestrian-traffic-signal',
+  initial:'red',
+ states:{ 
+   red:{
+    on:{NEXT:'green'},
+    //  entry: () => console.log(id), 
+     exit: () => console.log(`red out`),
+   },
+   green:{
+      on: {NEXT: 'red',
+      actions: () => console.log(`green out`),
+    },
+      
+     },
+ }//states	
+});
+
+const crossWalkMachine = createMachine({
+  id:'cross-walk-machine',
+  initial:'IDLE',
+context:{
+    carLight:null,
+    pedLight:null//spawn(pedestrianSignal)
+  },
+states:{
+    IDLE:{
+      entry: assign({
+        pedLight: () => spawn(pedestrianSignal),
+        carLight: () => spawn(trafficSignalMachine)
+      }),
+      on:{
+         NEXT:{
+          // actions: 'pedLightNEXT',
+          // entry: 'pedLightNEXT',
+          // entry: send({ type: 'NEXT' }, {to:(ctx) => ctx.pedLight}),
+          actions: send({ type: 'NEXT' },{to:(ctx) => ctx.pedLight}),
+          // entry: send({ type: 'NEXT' }, {to:'ped'}),
+          // actions: send({ type: 'NEXT' },{to:(ctx) => ctx.pedLight}),
+          target:'pedestrian_walk'
+         },
+       },
+    },
+    pedestrian_walk:{
+   //     //pedestrianSignal.send('NEXT')
+   //     on: {NEXT: 'pedestrian_stop'}
+      },
+   // pedestrian_stop:{
+   //     //pedestrianSignal.send('NEXT')
+   //     on: {NEXT: 'car_standby'}
+   //    },
+   // car_standby:{
+   //   //trafficSignalMachine.send('NEXT')
+   //   on: {NEXT: 'car_go'}
+   //   },
+   // car_go:{
+   //     //trafficSignalMachine.send('NEXT')
+   //     on: {NEXT: 'car_stop'}
+   //   },
+   // car_stop:{
+   //     //trafficSignalMachine.send('NEXT')
+   //     on:{NEXT:'pedestrian_walk'}
+   //   },
+}},
+{
+  actions:{
+  pedLightNEXT: (ctx,e) => {
+    send({ type: 'NEXT' },{to:(ctx) => ctx.pedLight})
+    },
+  }
+}
+);
+
+let appData = createContext() // you can optionally pass it a default value // it returns a "provider" object 
 
 function App() {
-  return (<>
-    <TrafficSignal/>
-    <PedestrianTrafficSignal/>
-    </>);
+
+  const [crossState,sendCrossState] = useMachine(crossWalkMachine);
+  // const [signal, send] = useMachine(pedestrianSignal);
+  // console.dir(pedestrianSignal)
+  // console.dir(twoLights?.context?.pedLight?.machine)
+  // console.dir(signal)
+  // const [signal, send] = useMachine(twoLights.context.pedLight.machine);
+  // const state = crossWalkMachine.transition(crossWalkMachine.initialState, { type: 'IDLE' });
+  // console.log(`state.value-->${state.value}`)
+  // console.dir(crossState)
+  return <CrossWalk state={crossState} send={sendCrossState}/>
 }
 
 export default App;
--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,130 +1,79 @@
 import React from 'react'
-import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
-import '@fortawesome/fontawesome-svg-core'
-import 'font-awesome/css/font-awesome.min.css';
+// import '@fortawesome/fontawesome-svg-core'
+// import 'font-awesome/css/font-awesome.min.css';
 import { ReactComponent as StandIcon } from "./resources/person-solid.svg";
 import { ReactComponent as WalkIcon } from "./resources/person-walking-solid.svg";
 
 
-const trafficSignalMachine = createMachine({
-  id:'traffic-signal',
-  initial:'red',
-	states:{ 
-		red:{
-			on:{NEXT:'orange'},
-		},
-		orange:{
-      on: {NEXT: 'green'}
-     },
-		green:{
-			 on: {NEXT: 'orange_reverse'},
-       entry: 'alertGreen'
-			},
-    orange_reverse:{
-        on: {NEXT: 'red'},
-      },
-	}//states	
-},
-{
-  actions: {
-    // action implementation
-    alertGreen: (context, event) => {
-      // alert('Green!');
-    }
-  }
-}  
-);
-
-
-export const TrafficSignal = () => {
-
-  const [signal, nextSignal] = useMachine(trafficSignalMachine);
-
+export const TrafficSignal = ({state,send}) => {
 
   return (<>
   <div style={{display:'flex',flexDirection:'column',gap:'5px',padding:'5px'}}>
-    { !signal.matches('red') &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-    { signal.matches('red') &&<div style={{backgroundColor:'red',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-    { !['orange','orange_reverse'].some(signal.matches) &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-    { ['orange','orange_reverse'].some(signal.matches) &&<div style={{backgroundColor:'orange',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-    { !signal.matches('green') && <div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
-    { signal.matches('green') && <div style={{backgroundColor:'green',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
+    { !state.matches('red') &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
+    { state.matches('red') &&<div style={{backgroundColor:'red',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
+    { !['orange','orange_reverse'].some(state.matches) &&<div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
+    { ['orange','orange_reverse'].some(state.matches) &&<div style={{backgroundColor:'orange',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
+    { !state.matches('green') && <div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
+    { state.matches('green') && <div style={{backgroundColor:'green',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
 
+  <button onClick={() => send('NEXT')}>Next Signal</button>
   </div>
 
-  <button onClick={() => nextSignal('NEXT')}>Next Signal</button>
+  
   </>)
 }
 
-const pedestrianSignal = createMachine({
-  id:'pedestrian-traffic-signal',
-  initial:'red',
- states:{ 
-   red:{
-     on:{NEXT:'green'}
-   },
-   green:{
-      on: {NEXT: 'red'}
-     },
- }//states	
-});
-
-
-const crossWalkMachine = createMachine({
- id:'cross-walk-machine',
- initial:'stop',
- states:{ 
-   stop:{
-     on:{NEXT:'pedestrian_walk'}
-   },
-   pedestrian_walk:{
-      //pedestrianSignal.send('NEXT')
-      on: {NEXT: 'pedestrian_stop'}
-     },
-  pedestrian_stop:{
-      //pedestrianSignal.send('NEXT')
-      on: {NEXT: 'car_standby'}
-     },
-  car_standby:{
-    //trafficSignalMachine.send('NEXT')
-    on: {NEXT: 'car_go'}
-    },
-  car_go:{
-      //trafficSignalMachine.send('NEXT')
-      on: {NEXT: 'car_stop'}
-    },
-  car_stop:{
-      //trafficSignalMachine.send('NEXT')
-      on:{NEXT:'pedestrian_walk'}
-    },
-
- }//states	
-});
-
-
-export const PedestrianTrafficSignal = () => {
-
- const [signal, nextSignal] = useMachine(pedestrianSignal);
+export const PedestrianTrafficSignal = ({state,send}) => {
+ 
+//  const [crossWalk, sendCrossWalk] = useMachine(crossWalkMachine);
+//  const [signal, nextSignal] = useMachine(crossWalk.context.pedLight);
+  // const [signal, send] = useMachine(machine);
+  // console.log(`ped.signal.value-->${signal.value}`);
+//  crossWalk.context.pedLight
+//  console.dir(crossWalk.context.pedLight)
+ //pedLight
 
 
  return (<>
 
  <div style={{display:'flex',flexDirection:'column',gap:'5px',padding:'5px'}}>
-   { !signal.matches('red') &&
+   { !state.matches('red') &&
       <div style={{color:'white',backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div>}
-   { signal.matches('red') &&
+   { state.matches('red') &&
      <div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}>
        <StandIcon style={{width:'40px',height:'40px',color:'red',marginLeft:'5px',marginTop:'5px'}} fill="red"/>
      </div>}
-   { !signal.matches('green') && 
+   { !state.matches('green') && 
      <div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}></div> }
-   { signal.matches('green') && 
+   { state.matches('green') && 
      <div style={{backgroundColor:'black',width:'50px',height:'50px',borderRadius:'50px'}}>
        <WalkIcon style={{width:'40px',height:'40px',color:'green',marginLeft:'5px',marginTop:'5px'}} fill="green"/>
      </div> }
+<button onClick={() => send('NEXT')}>Next Signal</button>     
 </div>
-
- <button onClick={() => nextSignal('NEXT')}>Next Signal</button>
  </>)
+}
+
+
+export const CrossWalk = ({state,send}) => {
+
+  const [pedState, sendPed] = useMachine(state.context.pedLight.state.machine);
+  const [carState, sendCar] = useMachine(state.context.carLight.state.machine);
+
+  console.log(`state.context.pedLight.state.id-->${state.context.pedLight.id}`)
+   
+  const render = () => { 
+    return (<>
+        <div style={{display:'flex',flexDirection:'row'}}>
+          <PedestrianTrafficSignal state={pedState} send={sendPed}/>
+          <TrafficSignal state={carState} send={sendCar}/>
+          <PedestrianTrafficSignal state={pedState} send={sendPed}/>
+        </div>
+
+      <button onClick={() => send('NEXT')}>Next</button>     
+      </>);
+   }
+
+  return render()
 }
